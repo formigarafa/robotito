ed70cfc58f93ee413ad8d03f2a8f0a43d675891c
Rafael Santos <rafaelsantos@xlii.com.br>
2011-04-02 23:36:23 -0300

    changed credentials file name

--- lib/boot.rb
+++ lib/boot.rb
@@ -1,4 +1,4 @@
-JabberShellRoot = File.dirname(__FILE__)
+JabberShellRoot = ::File.expand_path('../..',  __FILE__)
 
 require 'rubygems'
 require 'bundler/setup'
@@ -6,6 +6,6 @@ require 'xmpp4r-simple'
 require 'session'
 require 'daemons'
 
-require "#{JabberShellRoot}/gems_patch"
-require "#{JabberShellRoot}/../config"
-require "#{JabberShellRoot}/jabber_shell"
+require "#{JabberShellRoot}/config/credentials"
+require "#{JabberShellRoot}/lib/gems_patch"
+require "#{JabberShellRoot}/lib/jabber_shell"
