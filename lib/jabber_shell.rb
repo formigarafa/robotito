fa568ef4c3a5d975e4a4dcd3e5309204517c4595
Rafael Santos <rafaelsantos@xlii.com.br>
2011-04-04 23:20:33 -0300

    extracting main loop logic

--- lib/jabber_shell.rb
+++ lib/jabber_shell.rb
@@ -22,54 +22,70 @@ class JabberShell
   
   def processing_loop
     @sh = {}
-    poweron = true
+    power_on
 
-    while poweron
-      messenger.received_messages do |msg|  
-        puts "Received #{msg.body} from #{msg.from}"
-        from_name = msg.from.to_s.slice(0,msg.from.to_s.index('/')).sub('/', '')
-        if msg && AllowsedUsers.include?(from_name)
-          if msg.body == CLIENT_PASSPHRASE
-            if @sh[from_name].nil?
-              @sh[from_name] = Session::new 
-              messenger.deliver(msg.from, "Now logged in!")
-            else
-              messenger.deliver(msg.from, "Already logged in...")
-            end
-          elsif msg.body == 'jabberhelp'
-            messenger.deliver(msg.from, 'jabberhelp: this message
-            jabberw: users connected to a shell
-            jabberpoweroff: power this robot off
-            logmein: connect to shell
-            exit: close the shell connection')
-          elsif msg.body == 'jabberw'
-            messenger.deliver(msg.from, @sh.keys.join(', '))
-          elsif msg.body == 'exit' and @sh[from_name] != nil
+    while powered_on?
+      messenger.received_messages do |message|
+        process_message message
+      end
+      sleep 1
+    end
+  end
+  
+  def powered_on?
+    @powered_on
+  end
+  
+  def power_on
+    @powered_on = true
+  end
+  
+  def power_off
+    @powered_on = false
+  end
+  
+  def process_message(message)
+    puts "Received #{message.body} from #{message.from}"
+    from_name = message.from.to_s.slice(0,message.from.to_s.index('/')).sub('/', '')
+    if message && AllowsedUsers.include?(from_name)
+      if message.body == CLIENT_PASSPHRASE
+        if @sh[from_name].nil?
+          @sh[from_name] = Session::new 
+          messenger.deliver(message.from, "Now logged in!")
+        else
+          messenger.deliver(message.from, "Already logged in...")
+        end
+      elsif message.body == 'jabberhelp'
+        messenger.deliver(message.from, 'jabberhelp: this message
+        jabberw: users connected to a shell
+        jabberpoweroff: power this robot off
+        logmein: connect to shell
+        exit: close the shell connection')
+      elsif message.body == 'jabberw'
+        messenger.deliver(message.from, @sh.keys.join(', '))
+      elsif message.body == 'exit' and @sh[from_name] != nil
+        @sh[from_name].close && @sh[from_name] = nil
+        messenger.deliver(message.from, "Logged out...")
+      elsif message.body == 'jabberpoweroff' and @sh[from_name].nil?
+        messenger.deliver(message.from, "Power-off the bot!")
+        power_off
+      else
+        if @sh[from_name]
+          begin
+            stdout, stderr = @sh[from_name].execute(message.body) if message.body
+            messenger.deliver(message.from, "\n" + stdout.chomp) unless stdout.empty?
+            messenger.deliver(message.from, "\n" + stderr.chomp) unless stderr.empty?
+            messenger.deliver(message.from, @sh[from_name].execute('pwd')[0].chomp + "$>")
+          rescue
+            messenger.deliver(message.from, "Error: Shell aborted.\nCommand received:"+message.body)
             @sh[from_name].close && @sh[from_name] = nil
-            messenger.deliver(msg.from, "Logged out...")
-          elsif msg.body == 'jabberpoweroff' and @sh[from_name].nil?
-            messenger.deliver(msg.from, "Power-off the bot!")
-            poweron = false
-          else
-            if @sh[from_name]
-              begin
-                stdout, stderr = @sh[from_name].execute(msg.body) if msg.body
-                messenger.deliver(msg.from, "\n" + stdout.chomp) unless stdout.empty?
-                messenger.deliver(msg.from, "\n" + stderr.chomp) unless stderr.empty?
-                messenger.deliver(msg.from, @sh[from_name].execute('pwd')[0].chomp + "$>")
-              rescue
-                messenger.deliver(msg.from, "Error: Shell aborted.\nCommand received:"+msg.body)
-                @sh[from_name].close && @sh[from_name] = nil
-              end
-            else
-              messenger.deliver(msg.from, 'Not Authenticated!')
-            end
           end
         else
-          messenger.deliver(msg.from, "I don't talk to strangers!\nUser not allowed: "+from_name)
+          messenger.deliver(message.from, 'Not Authenticated!')
         end
-      end  
-      sleep 1  
+      end
+    else
+      messenger.deliver(message.from, "I don't talk to strangers!\nUser not allowed: "+from_name)
     end
   end
 end
