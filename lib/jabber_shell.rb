9be23855341450a212d68095cf5402808a9bdffc
Rafael Santos <rafaelsantos@xlii.com.br>
2011-04-04 23:39:25 -0300

    extracting processing message method

--- lib/jabber_shell.rb
+++ lib/jabber_shell.rb
@@ -46,46 +46,50 @@ class JabberShell
   
   def process_message(message)
     puts "Received #{message.body} from #{message.from}"
-    from_name = message.from.to_s.slice(0,message.from.to_s.index('/')).sub('/', '')
-    if message && AllowsedUsers.include?(from_name)
-      if message.body == CLIENT_PASSPHRASE
-        if @sh[from_name].nil?
-          @sh[from_name] = Session::new 
-          messenger.deliver(message.from, "Now logged in!")
-        else
-          messenger.deliver(message.from, "Already logged in...")
-        end
-      elsif message.body == 'jabberhelp'
-        messenger.deliver(message.from, 'jabberhelp: this message
-        jabberw: users connected to a shell
-        jabberpoweroff: power this robot off
-        logmein: connect to shell
-        exit: close the shell connection')
-      elsif message.body == 'jabberw'
-        messenger.deliver(message.from, @sh.keys.join(', '))
-      elsif message.body == 'exit' and @sh[from_name] != nil
-        @sh[from_name].close && @sh[from_name] = nil
-        messenger.deliver(message.from, "Logged out...")
-      elsif message.body == 'jabberpoweroff' and @sh[from_name].nil?
-        messenger.deliver(message.from, "Power-off the bot!")
-        power_off
-      else
-        if @sh[from_name]
-          begin
-            stdout, stderr = @sh[from_name].execute(message.body) if message.body
-            messenger.deliver(message.from, "\n" + stdout.chomp) unless stdout.empty?
-            messenger.deliver(message.from, "\n" + stderr.chomp) unless stderr.empty?
-            messenger.deliver(message.from, @sh[from_name].execute('pwd')[0].chomp + "$>")
-          rescue
-            messenger.deliver(message.from, "Error: Shell aborted.\nCommand received:"+message.body)
-            @sh[from_name].close && @sh[from_name] = nil
-          end
-        else
-          messenger.deliver(message.from, 'Not Authenticated!')
-        end
-      end
+    master_name = message.from.to_s.slice(0,message.from.to_s.index('/')).sub('/', '')
+    if message && AllowsedUsers.include?(master_name)
+      process_command(message.body, master_name, message)
     else
-      messenger.deliver(message.from, "I don't talk to strangers!\nUser not allowed: "+from_name)
+      messenger.deliver(message.from, "I don't talk to strangers!\nUser not allowed: "+master_name)
+    end
+  end
+  
+  def process_command(command, master_name, message)
+    if command == CLIENT_PASSPHRASE
+      if @sh[master_name].nil?
+        @sh[master_name] = Session::new 
+        messenger.deliver(message.from, "Now logged in!")
+      else
+        messenger.deliver(message.from, "Already logged in...")
+      end
+    elsif command == 'jabberhelp'
+      messenger.deliver(message.from, 'jabberhelp: this message
+      jabberw: users connected to a shell
+      jabberpoweroff: power this robot off
+      logmein: connect to shell
+      exit: close the shell connection')
+    elsif command == 'jabberw'
+      messenger.deliver(message.from, @sh.keys.join(', '))
+    elsif command == 'exit' and @sh[master_name] != nil
+      @sh[master_name].close && @sh[master_name] = nil
+      messenger.deliver(message.from, "Logged out...")
+    elsif command == 'jabberpoweroff' and @sh[master_name].nil?
+      messenger.deliver(message.from, "Power-off the bot!")
+      power_off
+    else
+      if @sh[master_name]
+        begin
+          stdout, stderr = @sh[master_name].execute(command) if command
+          messenger.deliver(message.from, "\n" + stdout.chomp) unless stdout.empty?
+          messenger.deliver(message.from, "\n" + stderr.chomp) unless stderr.empty?
+          messenger.deliver(message.from, @sh[master_name].execute('pwd')[0].chomp + "$>")
+        rescue
+          messenger.deliver(message.from, "Error: Shell aborted.\nCommand received:"+command)
+          @sh[master_name].close && @sh[master_name] = nil
+        end
+      else
+        messenger.deliver(message.from, 'Not Authenticated!')
+      end
     end
   end
 end
