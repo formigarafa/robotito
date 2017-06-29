6803f02faf3ee8e2b652a01f028f32495812b232
Rafael Santos <formigarafa@gmail.com>
2017-06-29 22:46:54 +1200

    added states to message processor

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "http://rubygems.org"
 gem 'xmpp4r-simple', :github => "blaine/xmpp4r-simple"
 gem 'session'
 gem 'daemons'
+gem 'state_machines'
--- lib/message_processor.rb
+++ lib/message_processor.rb
@@ -1,9 +1,34 @@
+require 'state_machines'
+
 module Robotito
   class MessageProcessor
     attr_accessor :jid
 
     def initialize(jid)
       self.jid = jid
+      super()
+    end
+
+    state_machine :state, initial: :waiting_for_identification do
+      event :identify do
+        transition :waiting_for_identification => :waiting_for_authentication
+      end
+
+      event :authenticate do
+        transition :waiting_for_authentication => :idle
+      end
+
+      event :work do
+        transition :idle => :working
+      end
+
+      event :cancel do
+        transition :idle => same, :working => :idle
+      end
+
+      event :logout do
+        transition :idle => :waiting_for_identification
+      end
     end
 
     def process(message)
