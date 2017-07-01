ebf14b6306ea86beb978b39573e4651503f5c18d
Rafael Santos <formigarafa@gmail.com>
2017-07-01 21:58:03 +1200

    processing commands according with states

--- lib/message_processor.rb
+++ lib/message_processor.rb
@@ -2,47 +2,42 @@ require 'state_machines'
 
 module Robotito
   class MessageProcessor
-    attr_accessor :jid
+    attr_accessor :jid, :user_id
 
     def initialize(jid)
       self.jid = jid
       super()
     end
 
-    state_machine :state, initial: :waiting_for_identification do
+    state_machine :state, initial: :identification do
       event :identify do
-        transition :waiting_for_identification => :waiting_for_authentication
+        transition :identification => :authentication
+      end
+
+      event :unidentify do
+        transition :authentication => :identification
       end
 
       event :authenticate do
-        transition :waiting_for_authentication => :idle
+        transition :authentication => :shell
       end
 
       event :work do
-        transition :idle => :working
+        transition :shell => :busy
       end
 
-      event :cancel do
-        transition :idle => same, :working => :idle
+      event :rest do
+        transition :busy => :shell
       end
 
       event :logout do
-        transition :idle => :waiting_for_identification
-      end
-      event :noop do
-        transition all => same
+        transition :shell => :identification
       end
     end
 
     def process(message)
-      command_data = parse_command(message.body)
-      str = if ! command_data[:valid]
-        "command invalid: #{command_data.inspect}"
-      elsif command_data[:internal]
-        send command_data[:command]
-      else
-        "not Implemented"
-      end
+      # command_data = parse_command(message.body)
+      str = send("#{state_name}_command", message.body)
       if block_given?
         yield <<~MESSAGE
           Processor [#{self.object_id}]: state {#{self.state_name.inspect}}
@@ -54,15 +49,34 @@ module Robotito
 
     private
 
-    def parse_command(message)
-      first_line = message.lines.map(&:strip).reject(&:empty?).first || '#!noop'
-      command = (first_line[2..-1].split(" ").first || 'noop').to_sym
-      internal = first_line[0..1] == '#!'
-      {
-        internal: internal,
-        command: command,
-        valid: !internal || state_events.include?(command),
-      }
+    def sanitize_message(message)
+      # first_line = message.lines.map(&:strip).reject(&:empty?).first
+      # command = (first_line[2..-1].split(" ").first || 'noop').to_sym
+      # internal = first_line[0..1] == '#!'
+      # {
+      #   internal: internal,
+      #   command: command,
+      #   valid: !internal || state_events.include?(command),
+      # }
     end
+
+    def first_word(message)
+      message.lines.map(&:strip).reject(&:empty?).first
+    end
+
+    def identification_command(message)
+    end
+
+    def authentication_command(message)
+    end
+
+    def shell_command(message)
+      "shell #{message}"
+    end
+
+    def busy_command(message)
+      "busy #{message}"
+    end
+
   end
 end
